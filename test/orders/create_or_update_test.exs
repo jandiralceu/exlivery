c92aca406c0a1c417bc923d01160ef7e4af83caf
Jandir A. Cutabiala <jandir.cutadiala@genial.com.vc>
2023-08-02 00:45:31 -0300

    test: create and update order

--- test/orders/create_or_update_test.exs
+++ test/orders/create_or_update_test.exs
@@ -53,5 +53,25 @@ defmodule Exlivery.Orders.CreateOrUpdateTest do
 
       assert response == {:error, "user not found"}
     end
+
+    test "should return an error if has invalid items", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
+      params = %{user_cpf: cpf, items: [%{item1 | quantity: 0}, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert response == {:error, "Invalid struct"}
+    end
+
+    test "should return an error if the items list is empty", %{user_cpf: cpf} do
+      params = %{user_cpf: cpf, items: []}
+
+      response = CreateOrUpdate.call(params)
+
+      assert response == {:error, "Invalid parameters"}
+    end
   end
 end
